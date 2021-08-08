58704bfba394a27e49c922711bb473acce9c3a3b
phanikiranthaticharla <t.phani.kiran@gmail.com>
2021-08-07 17:50:26 -0700

    Add service annotation to UserService class

--- src/main/java/com/techprimers/cache/springredisexample/service/UserService.java
+++ src/main/java/com/techprimers/cache/springredisexample/service/UserService.java
@@ -1,9 +1,11 @@
 package com.techprimers.cache.springredisexample.service;
 
 import com.techprimers.cache.springredisexample.model.User;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
+@Service
 public interface UserService {
 
     void save(User user);
